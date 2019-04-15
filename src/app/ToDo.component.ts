2ef897d012432394fb41caefa3264124a2466dfb
Brendan525 <broe5252@hotmail.com>
2019-04-15 18:35:47 -0400

    First Commit

---
+++ src/app/ToDo.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+interface ToDo  {
+  chore: string;
+  complete: boolean; 
+  hideTitle: boolean;
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './ToDo.component.html',
+  styleUrls: ['./ToDo.component.css']
+})
+export class AppComponent {
+  choreInput: string;
+  completeClick: boolean= true;
+  stylesList: boolean= true;
+
+
+ choreList: ToDo[] =  [
+    {chore: 'Brushing teeth and wash face', complete: true}, 
+    {chore: 'Iron clothes', complete: false},
+    {chore: 'Cook and eat breakfast', complete: true},
+    {chore: 'Go to work', complete: false},
+  ];
+
+  addTask = () => {
+    const newChore = {
+      chore: this.choreInput,
+    };
+    this.choreList.push(newChore);
+    this.choreInput = null;
+   }
+
+  completeTask = (index) => {
+    // this.stylesList = !this.stylesList;
+
+    this.choreList.splice(index,1)
+  }
+   
+// toggleHeader= () => {
+//   this.stylesList = !this.stylesList;
+// };
+
+// toggleStrike= () => {
+//   this.hideTitle = !this.hideTitle;
+// }
+
+removeTask = (index) =>  {
+  this.choreList.splice(index,1);
+  // this.choreInput = null;
+};
+
+togglestylesList= () => {
+   stylesList = !stylesList;
+}
+
+}
---
+++ src/app/ToDo.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{ FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './ToDo-routing.module';
+import { AppComponent } from './ToDo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
